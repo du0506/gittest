23c53df462419c93d697deb1105ac6d185e19274
du0506 <1449217789@qq.com>
2020-09-28 09:57:57 +0800

    Create modify_disk.sh

---
+++ modify_disk.sh
@@ -0,0 +1,15 @@
+#aliyun
+
+echo "格式化新分区"
+mkfs -t ext4 /dev/vdb1
+
+echo "将分区写入 /etc/fstab"
+cp /etc/fstab /etc/fstabbak
+echo `blkid /dev/vdb1 | awk '{print $2}' | sed 's/\"//g'` /mnt ext4 defaults 0 0 >> /etc/fstab
+df -h
+
+echo "将新分区挂载到 /mnt "
+mount /dev/vdb1 /mnt
+
+echo "查看最终结果"
+df -h
